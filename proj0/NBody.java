e644185134f4f8dd9d54f55682830a2c01401bb0
town <603368546@qq.com>
2022-12-05 17:14:54 +0800

    the audio doesnot work

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -32,6 +32,8 @@ public class NBody {
         String filename = args[2];
         double radius = readRadius(filename);
         Planet[] planets = readPlanets(filename);
+        String music = "audio/2001.mid";
+        StdAudio.play(music);
         StdDraw.setScale(-radius,radius);
         StdDraw.clear();
         String background = "./images/starfield.jpg";
@@ -68,7 +70,7 @@ public class NBody {
         }
         StdOut.printf("%d\n", planets.length);
         StdOut.printf("%.2e\n", radius);
-        for (int i = 0; i < planets.length; i++) {
+        for (i = 0; i < planets.length; i++) {
             StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
                     planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
                     planets[i].yyVel, planets[i].mass, planets[i].imgFileName);
